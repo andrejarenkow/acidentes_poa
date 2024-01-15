7370e147c4e199349f10718ee480a89aa3e0f30d
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-01-15 16:56:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,7 @@ with tab_scatter:
                             zoom = 9.5,
                             mapbox_style = 'open-street-map',
                             color_continuous_scale = 'turbo',
+                            color = 'ups',
                             opacity = 0.6,
                             center=dict(lat=-30.085815797161448 , lon= -51.17306247847506),
                             height=600)
