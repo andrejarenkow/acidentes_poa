39e843fdbae34e215f2796aa281edf02a8adba62
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-01-15 01:04:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,29 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state='collapsed'
 )
+
+@st.cache_data(ttl='24h')
+def load_data():
+    acidentes_poa = pd.read_csv('https://dadosabertos.poa.br/dataset/d6cfbe48-ee1f-450f-87f5-9426f6a09328/resource/b56f8123-716a-4893-9348-23945f1ea1b9/download/cat_acidentes.csv', sep=';')
+    #puxando diretamente do site, o banco de dados fica sempre atualizado
+    acidentes_poa.drop(['data_extracao', 'idacidente' ], axis=1, inplace=True) #limpando o dataframe
+    
+    acidentes_poa['data'] = pd.to_datetime(acidentes_poa['data']) #passando para formato de data
+    acidentes_poa['horario'] = pd.to_datetime(acidentes_poa['hora']).dt.time
+    
+    acidentes_poa['hora'] = pd.to_datetime(acidentes_poa['hora']).dt.hour
+    
+    acidentes_poa['ano'] = pd.DatetimeIndex(acidentes_poa['data']).year
+    acidentes_poa['mes'] = pd.DatetimeIndex(acidentes_poa['data']).month
+    
+    acidentes_poa['log1'] = acidentes_poa['log1'].str.strip()
+    
+    acidentes_poa['log2'] = acidentes_poa['log2'].str.strip()
+
+    acidentes_poa['cruzamento'] = acidentes_poa['log2'].notna()
+    
+    return acidentes_poa
+
+df = load_data()
+
+df
