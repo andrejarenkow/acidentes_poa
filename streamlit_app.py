b85eb255bb584cbdf7430d03684855b617adcce3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-01-15 00:42:33 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as st
+import numpy as np
+
+# Configurações da página
+st.set_page_config(
+    page_title="Buscador de Soros Antiveneno no Rio Grande do Sul",
+    page_icon="	:snake:",
+    layout="wide",
+    initial_sidebar_state='collapsed'
+)
